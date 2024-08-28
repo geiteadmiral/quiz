1b1f4aca1fe30f267950c0b735e0066ed85e09c3
geiteadmiral <silje.hennie.johansen@gmail.com>
2024-08-28 15:30:55 +0200

    fixed sidebar

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-/* Place your JavaScript in this file */
+//dette må endres fordi 
+let open = document.getElementById("openmenu")
+let close = document.getElementById("closemenu")
+
+let sidebar = document.getElementById("sidebar")
+
+open.addEventListener("click", openFunk)
+
+function openFunk() {
+    sidebar.style.display = "flex";
+    sidebar.style.flexDirection = "column";
+}
+
+close.addEventListener("click", closeFunk)
+
+function closeFunk() {
+    sidebar.style.display = "none";
+}
